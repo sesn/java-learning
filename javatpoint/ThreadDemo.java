413c759a848b2cffba72b37d528a3fc947a1aa22
sesn <sesankar11@gmail.com>
2018-05-05 11:42:55 +0530

    Runnable thread implementation

--- javatpoint/ThreadDemo.java
+++ javatpoint/ThreadDemo.java
@@ -19,3 +19,22 @@ class NewThread implements Runnable {
         System.out.println("Exiting child thread.");
     }
 }
+
+
+class ThreadDemo {
+    public static void main(String args[]) {
+        NewThread nt = new NewThread();
+
+        nt.t.start(); // Start the thread
+
+        try {
+            for(int i=5; i>0; i--) {
+                System.out.println("Main Thread: "+i);
+                Thread.sleep(500);
+            }
+        } catch(InterruptedException e) {
+            System.out.println("Main thread Interrupted.");
+        }
+        System.out.println("Main Thread exiting.");
+    }
+}
