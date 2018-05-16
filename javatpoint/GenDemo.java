6764edfaa28eaec421fb0f1cccdd9737738879ce
sesn <sesankar11@gmail.com>
2018-05-16 14:04:32 +0530

    generics demo

---
+++ javatpoint/GenDemo.java
@@ -0,0 +1,40 @@
+class Gen<T> {
+    T ob;
+
+    Gen(T o) {
+        ob = o;
+    }
+
+    T getob() {
+        return ob;
+    }
+
+    void showType() {
+        System.out.println("Type of T is " + ob.getClass().getName());
+    }
+}
+
+
+class GenDemo {
+    public static void main(String args[]) {
+        Gen<Integer> iOb;
+
+        iOb = new Gen<Integer>(88);
+
+        iOb.showType();
+
+        int v = iOb.getob();
+        System.out.println("value: " +  v);
+
+        System.out.println();
+
+        Gen<String> strOb;
+
+        strOb = new Gen<String> ("Generics Test");
+
+        strOb.showType();
+
+        String str = strOb.getob();
+        System.out.println("value: "+ str);
+    }
+}
